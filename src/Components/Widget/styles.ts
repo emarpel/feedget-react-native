77866a968a100bd9dfb5bf30bee7441ab7a84ce4
David Marcelo Gois Silva <emarpel@gmail.com>
2022-05-11 09:07:48 -0300

    upload

--- src/Components/Widget/styles.ts
+++ src/Components/Widget/styles.ts
@@ -2,11 +2,11 @@ import { StyleSheet } from 'react-native';
 import { getBottomSpace } from 'react-native-iphone-x-helper';
 import { theme } from '../../theme';
 
-export const styles = StyleSheet.create({
+export const styles = StyleSheet.create({  
   button: {
-    width: 50,
-    height: 50,
-    borderRadius: 25,
+    width: 75,
+    height: 75,
+    borderRadius: 50,
     backgroundColor: theme.colors.brand,
     justifyContent: 'center',
     alignItems: 'center',
@@ -14,10 +14,12 @@ export const styles = StyleSheet.create({
     right: 16,
     bottom: getBottomSpace() + 16,
   },
+
   modal: {
      backgroundColor: theme.colors.surface_primary,
      paddingBottom: getBottomSpace() + 16,
   },
+
   indicator: {
     backgroundColor: theme.colors.text_primary,
     width: 56,
